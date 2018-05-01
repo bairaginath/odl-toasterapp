761bdeb791ab070546a94d9f6a61c201256e5081
bairagi <bairagi.java@gmail.com>
2018-05-02 03:27:36 +0530

    delete target folder

--- clustering-test-app/model/target/generated-sources/mdsal-binding/org/opendaylight/yang/gen/v1/tag/opendaylight/org/_2017/controller/yang/lowlevel/common/rev170215/$YangModelBindingProvider.java
+++
@@ -1,8 +0,0 @@
-package org.opendaylight.yang.gen.v1.tag.opendaylight.org._2017.controller.yang.lowlevel.common.rev170215;
-
-public final class $YangModelBindingProvider implements org.opendaylight.yangtools.yang.binding.YangModelBindingProvider {
-    @Override
-    public org.opendaylight.yangtools.yang.binding.YangModuleInfo getModuleInfo() {
-        return $YangModuleInfoImpl.getInstance();
-    }
-}
